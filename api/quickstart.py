a855655f1a2214a4a4d375c9fb4868b27dd26da4
Allen Canning <canning@nejll.org>
2018-07-18 09:43:04 -0400

    updated invoice_search

--- api/quickstart.py
+++ api/quickstart.py
@@ -4,28 +4,39 @@ Shows basic usage of the Sheets API. Prints values from a Google Spreadsheet.
 """
 from __future__ import print_function
 from apiclient.discovery import build
+from google.oauth2 import service_account
 from httplib2 import Http
 from oauth2client import file, client, tools
 
 # Setup the Sheets API
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SERVICE_ACCOUNT_FILE="service_secret.json"
 store = file.Storage('credentials.json')
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
-    creds = tools.run_flow(flow, store)
-service = build('sheets', 'v4', http=creds.authorize(Http()))
+    creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+service = build('sheets', 'v4', credentials=creds)
 
 # Call the Sheets API
 SPREADSHEET_ID = '1NY1Ue1OA_Yzm0lg7aG-ITH0hZSRdISe_ZhMlAtlXgXU'
-RANGE_NAME = 'A1:E'
-result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
-                                             range=RANGE_NAME).execute()
-values = result.get('values', [])
-if not values:
-    print('No data found.')
-else:
-    print('Name, Major:')
-    for row in values:
-        # Print columns A and E, which correspond to indices 0 and 4.
-        print('%s, %s' % (row[0], row[4]))
+RANGE_NAME = 'A2'
+value_input_option='USER_ENTERED'
+
+#result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
+#                                              range=RANGE_NAME).execute()
+#values = result.get('values', [])
+#if not values:
+#    print('No data found.')
+#else:
+#    print('Name, Major:')
+#    for row in values:
+#        # Print columns A and E, which correspond to indices 0 and 4.
+#        print('%s, %s' % (row[0], row[4]))
+
+values=[ [ 'More', 'test', 'data', 1 ] ]
+body = { 'values': values }
+
+result = service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID,
+                                                range=RANGE_NAME, valueInputOption=value_input_option, body=body).execute()
+
+print ('{0} cells updated.'.format(result.get('updatedCells')))
--- invoice_search.py
+++ invoice_search.py
@@ -1,8 +1,15 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
 
+from __future__ import print_function
+from apiclient.discovery import build
+from google.oauth2 import service_account
+from httplib2 import Http
+from oauth2client import file, client, tools
 import json
 import yaml
 import requests
+import time
+import collections
 from optparse import OptionParser
 
 def log_error(msg):
@@ -58,37 +65,64 @@ def getInvoices(environment,paypal):
   elif environment == "production":
     url = "https://api.paypal.com/v1/invoicing/search/"
 
-  data = { 'start_invoice_date': '2018-05-01 PDT',
-           'end_invoice_date': '2018-07-31 PDT',
-           'page_size': 100
-  }
+  data = { "status": ["SENT", "PAID"],
+           "invoice_date": "2018-06-01 PDT",
+           "page_size": 100 }
 
   #print ("Data = "+str(data))
-  print ("Calling URL = "+url)
+  #print ("Calling URL = "+url)
   r = requests.post(url,headers=headers,data=json.dumps(data))
 
   body = r.json()
-#  print("Body = "+r.text)
+  #print("Body = "+r.text)
 
   count = 0
   for invoice in body['invoices']:
+    #print(invoice)
     print(str(count)+" ID = "+invoice['id']+" status = "+invoice['status']+" Invoice date = "+invoice['invoice_date'])
     count = count + 1
 
-  print()
+  #print()
+  return body['invoices']
 
 def updateSheet(invoices):
   # Update Google sheet with invoice data
 
-  google_auth="nejllreports"
-  #for each invoice in invoices:
-    # Find in sheets
-    # Update status
+  # Setup the Sheets API
+  SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+  SERVICE_ACCOUNT_FILE="service_secret.json"
+
+  creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+  service = build('sheets', 'v4', credentials=creds)
+
+  # Call the Sheets API
+  SPREADSHEET_ID = '1NY1Ue1OA_Yzm0lg7aG-ITH0hZSRdISe_ZhMlAtlXgXU'
+  # always set to column 1
+  range = collections.defaultdict(dict)
+  range['SENT']['ROW'] = 1
+  range['PAID']['ROW'] = 1
+  COL = 'A'
+  value_input_option='USER_ENTERED'
+
+  # Clear the SENT sheet
+  result = service.spreadsheets().values().clear(spreadsheetId=SPREADSHEET_ID,range='SENT').execute()
+  # Clear the PAID sheet
+  result = service.spreadsheets().values().clear(spreadsheetId=SPREADSHEET_ID,range='PAID').execute()
+  
+  for invoice in invoices:
+    values = [ [ invoice['id'], invoice['status'], invoice['invoice_date'], invoice['billing_info'][0]['email'], invoice['total_amount']['value'] ] ]
+    body = { 'values': values }
+
+    result = service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID,
+                                                range=invoice['status']+'!'+COL+str(range[invoice['status']]['ROW']), valueInputOption=value_input_option, body=body).execute()
+    time.sleep(1)
+    range[invoice['status']]['ROW'] = range[invoice['status']]['ROW'] + 1
 
 # Begin main program
 parser = OptionParser()
 parser.add_option("-e","--environment", dest="environment", help="Paypal Environment -- OPTIONAL defaults to sandbox [ sandbox, production ]")
 parser.add_option("-f","--file",dest="credfile", help="Paypal Credential file (YAML) -- OPTIONAL defaults to paypal.yaml")
+parser.add_option("-v","--verbose",dest="verbose", help="Verbose")
 
 (options,args) = parser.parse_args()
 
@@ -102,7 +136,12 @@ if options.credfile:
 else:
   credfile = "paypal.yaml"
 
+if options.verbose:
+  verbose=True
+else:
+  verbose=False
+
 paypal = getPayPalToken(credfile,environment)
 invoices = getInvoices(environment,paypal)
-#updateSheet(invoices)
+updateSheet(invoices)
 
